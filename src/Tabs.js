231dc78b9b42361e42d8465ad737b75a0fbcf6f8
dpx16861 <dpx16861@gmail.com>
2017-03-28 01:12:13 +0300

    Refactor Tabs component

--- src/Tabs.js
+++ src/Tabs.js
@@ -13,27 +13,38 @@ class Tabs extends Component {
         });
     }
 
+    renderTitles() {
+        return (
+            <ul className="nav nav-tabs">
+                {
+                    this.props.children.map((child, index) => {
+                        const activeClass = (this.state.selected === index ? 'active' : '');
+                        return (
+                            <li className={activeClass} key={index}>
+                                <a href="#" onClick={(e) => this.handleClick(e, index)}>
+                                    {child.props.label}
+                                </a>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        );
+    }
+
+    renderContent() {
+        return (
+            <div className="tab-content">
+                {this.props.children[this.state.selected]}
+            </div>
+        );
+    }
+
     render() {
         return (
             <div className="tabs">
-                <ul className="nav nav-tabs">
-                    {
-                        this.props.children.map((child, index) => {
-                            const activeClass = (this.state.selected === index ? 'active' : '');
-                            return (
-                                <li className={activeClass} key={index}>
-                                    <a href="#" onClick={(e) => this.handleClick(e, index)}>
-                                        {child.props.label}
-                                    </a>
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-
-                <div className="tab-content">
-                    {this.props.children[this.state.selected]}
-                </div>
+                {this.renderTitles()}
+                {this.renderContent()}
             </div>
         )
     }
